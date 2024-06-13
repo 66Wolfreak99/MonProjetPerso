4441496e2e4255f35629a4cb8d1fd1b440e7d47c
66Wolfreak99 <jrmyargoud@gmail.com>
2024-06-13 13:57:08 +0200

    finished JS integration

--- src/script/posts.js
+++ src/script/posts.js
@@ -7,6 +7,12 @@ import pfp6 from "../img/pfp6.webp";
 
 import banner1 from "../img/banner.webp";
 
+import fox1 from "../img/fox1.webp";
+import fox2 from "../img/fox2.jpg";
+import fox3 from "../img/fox3.jpg";
+import fox4 from "../img/fox4.webp";
+import fox5 from "../img/fox5.jpg";
+
 
 
 class Post{
@@ -37,7 +43,7 @@ const currentUser = {
 };
 
 const users = [
-{
+{//0
     username: "Fruite",
     imgUrl: pfp3,
     bannerUrl:"",
@@ -45,7 +51,7 @@ const users = [
     following: 10,
     followers: 600
 },
-{
+{//1
     username: "Dergast",
     imgUrl: pfp2,
     bannerUrl:"",
@@ -53,7 +59,7 @@ const users = [
     following: 10,
     followers: 600
 },
-{
+{//2
     username: "Mobius 1",
     imgUrl: pfp5,
     bannerUrl:"",
@@ -61,7 +67,7 @@ const users = [
     following: 10,
     followers: 600
 },
-{
+{//3
     username: "Goobus",
     imgUrl: pfp4,
     bannerUrl:"",
@@ -69,7 +75,7 @@ const users = [
     following: 10,
     followers: 600
 },
-{
+{//4
     username: "Wanderlost",
     imgUrl: pfp6,
     bannerUrl:"",
@@ -80,41 +86,115 @@ const users = [
 ];
 
 const posts = [
-    {
-        content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium corporis reiciendis aspernatur explicabo quis quam officia officiis quisquam eligendi, perspiciatis nesciunt voluptatibus harum necessitatibus ex dolore repudiandae in voluptates expedita?",
-        tags: ["Tag", "Literature", "Violence"],
-        username: currentUser.username,
-        imgUrl: currentUser.imgUrl,
-        views: 1586,
-        responses: 5,
-        likes: 500,
-        date: "9:40 May 8 2078"
-    },
-
-    {
-        content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium corporis reiciendis aspernatur explicabo quis quam officia officiis quisquam eligendi, perspiciatis nesciunt voluptatibus harum necessitatibus ex dolore repudiandae in voluptates expedita?",
-        tags: ["Tag", "Literature", "Violence"],
-        username: currentUser.username,
-        imgUrl: currentUser.imgUrl,
-        views: 1586,
-        responses: 5,
-        likes: 500,
-        date: "9:40 May 8 2078"
-    },
+    
+    new Post(
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iure veritatis neque possimus fugit perferendis nihil et id voluptates assumenda ratione minima ullam sunt sed eveniet dolore autem maiores repellat!",
+        "",
+        users[3].username,
+        users[3].imgUrl,
+        ["Fooled", "AprilFools", "Aeruergh"],
+        3000,
+        35,
+        1300,
+        "7:30 April 1 2078"
+    ),
 
     new Post(
-        "This is hella rad!",
-        [banner1, banner1, banner1, banner1, banner1],
-        users[2].username,
-        users[2].imgUrl,
-        ["Dandes", "WTF", "Cute"],
-        858,
-        4,
-        78,
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iure veritatis neque possimus fugit perferendis nihil et id voluptates assumenda ratione minima ullam sunt sed eveniet dolore autem maiores repellat!",
+        "",
+        users[0].username,
+        users[0].imgUrl,
+        ["Fruits", "Cooking", "Recipes", "Chef"],
+        1716,
+        3,
+        121,
+        "16:01 April 25 2078"
+    ),
+
+    new Post(
+        "Family! ❤🦊",
+        [fox1, fox2, fox3, fox4, fox5],
+        users[4].username,
+        users[4].imgUrl,
+        ["Fox", "Fluffy"],
+        2520,
+        16,
+        1400,
+        "13:50 April 25 2078"
+    ),
+
+    new Post(
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium corporis reiciendis aspernatur explicabo quis quam officia officiis quisquam eligendi!",
+        "",
+        users[3].username,
+        users[3].imgUrl,
+        ["Tag", "Litterature", "Violence"],
+        708,
+        2,
+        121,
+        "8:00 May 6 2078"
+    ),
+
+    new Post(
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium corporis reiciendis aspernatur explicabo quis quam officia officiis quisquam eligendi!",
+        [banner1],
+        currentUser.username,
+        currentUser.imgUrl,
+        ["Banner", "Profile", "Dragons"],
+        480,
+        0,
+        50,
+        "14:55 May 5 2078"
+    ),
+
+    new Post(
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium corporis reiciendis aspernatur explicabo quis quam officia officiis quisquam eligendi!",
+        "",
+        currentUser.username,
+        currentUser.imgUrl,
+        ["Tag", "Litterature", "Violence"],
+        1586,
+        5,
+        500,
         "9:40 May 8 2078"
+    ),
+    
+
+    // {
+    //     content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium corporis reiciendis aspernatur explicabo quis quam officia officiis quisquam eligendi, perspiciatis nesciunt voluptatibus harum necessitatibus ex dolore repudiandae in voluptates expedita?",
+    //     tags: ["Tag", "Literature", "Violence"],
+    //     username: currentUser.username,
+    //     imgUrl: currentUser.imgUrl,
+    //     views: 1586,
+    //     responses: 5,
+    //     likes: 500,
+    //     date: "9:40 May 8 2078"
+    // },
+
+    // {
+    //     content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium corporis reiciendis aspernatur explicabo quis quam officia officiis quisquam eligendi, perspiciatis nesciunt voluptatibus harum necessitatibus ex dolore repudiandae in voluptates expedita?",
+    //     tags: ["Tag", "Literature", "Violence"],
+    //     username: currentUser.username,
+    //     imgUrl: currentUser.imgUrl,
+    //     views: 1586,
+    //     responses: 5,
+    //     likes: 500,
+    //     date: "9:40 May 8 2078"
+    // },
+
+    // new Post(
+    //     "This is hella rad!",
+    //     [banner1, banner1, banner1, banner1, banner1],
+    //     users[2].username,
+    //     users[2].imgUrl,
+    //     ["Dandes", "WTF", "Cute"],
+    //     858,
+    //     4,
+    //     78,
+    //     "9:40 May 8 2078"
 
 
-    )
+    // )
 ];
 
 export const postsList = {currentUser, users, posts}
